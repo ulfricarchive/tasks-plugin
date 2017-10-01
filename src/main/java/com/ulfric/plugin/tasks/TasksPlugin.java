3f7ffe2907646d5aa3c422ec661bf2402e54b436
Adam Harrison <ad@mharrison.us>
2017-10-01 13:32:25 -0400

    """IT COMPILES"""

--- src/main/java/com/ulfric/plugin/tasks/TasksPlugin.java
+++ src/main/java/com/ulfric/plugin/tasks/TasksPlugin.java
@@ -1,7 +1,13 @@
 package com.ulfric.plugin.tasks;
 
+import com.ulfric.commons.bukkit.plugin.PluginHelper;
 import com.ulfric.plugin.Plugin;
 
 public class TasksPlugin extends Plugin {
+
+	public TasksPlugin() { // TODO remove bindings on shutdown
+		Plugin.FACTORY.bind(Scheduler.class).toFunction(parameters ->
+			new Scheduler(PluginHelper.getProvidingPlugin(parameters.getHolder())));
+	}
 
 }
